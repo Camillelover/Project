6116e83592895beb26272f11b6fb771737c70615
Artem <example@gmail.com>
2021-02-07 20:45:31 +0300

    First commit

---
+++ script.js
@@ -0,0 +1,19 @@
+//alert(1);
+//let answer = +prompt("Est li vam 18","da");
+//console.log(typeof(answer));
+//console.log("arr" + "-object"); 
+let incr = 10,
+    decr = 10;
+
+
+
+console.log(++incr);
+console.log(--decr);
+console.log(5%2);
+console.log("2"==2);
+let isChecked = true,
+    isClose = true;
+    
+console.log(isChecked && isClose);
+
+
